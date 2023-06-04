9ebcfb908e599a49e20938d7e9f3f67538d059a8
0chuhh <vvlasovdo@mail.ru>
2023-06-04 20:19:55 +0900

    base components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Header from "./layout/header";
+import axios from "axios";
+import Tasks from "./modules/tasks";
 function App() {
+    const [tasks, setTasks] = useState([])
+    const fetchTasks = async () => {
+        const {data:Tasks}=await axios.get('http://127.0.0.1:8000/api/v1/tasks/', {
+            headers:{
+                'Authorization': 'Token d19f04f4206816f062a0cd4e65af91144a5219ac'
+            }
+        })
+        setTasks(Tasks)
+    }
+
+    useEffect(()=>{
+        fetchTasks()
+    }, [])
   return (
     <div className="App">
-  helpdesk
+      <Header/>
+    <div style={{marginTop:'20px'}}>
+        <Tasks tasks={tasks}/>    </div>
     </div>
   );
 }
